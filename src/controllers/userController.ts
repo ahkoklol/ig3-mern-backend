e5012a2ab003b25b05c577ec1796deeb80f8160d
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2024-02-01 16:00:03 +0100

    user id properly passed in response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,6 +7,7 @@ const createToken = (id: string) => {
     return token;
 }
 
+
 // Login user
 export const loginUser = async (req: Request, res: Response) => {
     const { email, password } = req.body;
@@ -14,11 +15,12 @@ export const loginUser = async (req: Request, res: Response) => {
     try {
         const user = await UserModel.login(email, password);
         const token = createToken(user.id);
-        res.status(200).json({ email, token });
+        res.status(200).json({ email, token, _id: user._id });
     } catch (error: any) {
         res.status(400).json({ error: error.message });
     }
 };
+
 
 // Signup user
 export const signupUser = async (req: Request, res: Response) => {
@@ -27,8 +29,41 @@ export const signupUser = async (req: Request, res: Response) => {
     try {
         const user = await UserModel.signup(email, password, name, surname, role);
         const token = createToken(user.id);
-        res.status(200).json({ email, token });
+        res.status(200).json({ email, token, _id: user._id });
     } catch (error: any) {
         res.status(400).json({ error: error.message });
     }
 };
+
+
+// Fetch and return the user's profile
+export const profileUser = async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+  
+    try {
+      // Fetch the user's profile based on their ID
+      const user = await UserModel.findById({_id: userId});
+  
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      // Extract relevant profile information
+      const profile = {
+        email: user.email,
+        name: user.name,
+        surname: user.surname,
+        role: user.role,
+        examsTaken: user.examsTaken,
+        progress: user.progress,
+        teachingClasses: user.teachingClasses,
+        assignedClasses: user.assignedClasses,
+        dateJoined: user.dateJoined,
+      };
+  
+      res.status(200).json(profile);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server error" });
+    }
+  };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 const router = Router();
 
 // Import the controller functions
-import { signupUser, loginUser } from "../controllers/userController";
+import { signupUser, loginUser, profileUser } from "../controllers/userController";
 
 // Login route
 router.post("/login", loginUser);
 
 // Signup route
 router.post("/signup", signupUser);
+
+// Profile route
+router.get("/profile/:userId", profileUser);
 
 export default router;
